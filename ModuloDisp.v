44bbcc6ee3fd134e437884f20191ce30436b49d5
Icaro G. S. Moreira <icaro.moreira@protonmail.com>
2019-09-22 00:36:21 -0300

    Update ModuloDisp.v

--- ModuloDisp.v
+++ ModuloDisp.v
@@ -7,7 +7,7 @@ module ModuloDisp(TOM, A, B, C, D, Saida_A, Saida_B, Saida_C, Saida_D, Saida_E, 
     assign Saida_B = (~A & B & ~C) | (~TOM & A & ~B) | (TOM & ~B & C) | (TOM & A & C);
     assign Saida_C = (C & ~TOM) | (~A & ~B & C) | (A & ~B & ~C);
     assign Saida_D = (B & TOM) | (A & TOM) | (A & B & ~C);
-    assign Saida_E = (B & C) | (~TOM & ~A & B | (A & ~B & ~C) | (TOM & A & ~B);
+    assign Saida_E = (B & C) | (~TOM & ~A & B) | (A & ~B & ~C) | (TOM & A & ~B);
     assign Saida_F = (~A & B & ~C) | (A & B & TOM) | (A & B & C) | (A & C & B) | (A & ~B & ~C & ~TOM) | (~A & ~B & C & ~TOM);
     assign Saida_G = (~A & C) | (B & ~C) | (TOM & A & ~B); 
 
